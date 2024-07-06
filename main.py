f4348955f9477e42c28f1212d5252e3f271f147f
mbledkowski <pub@mble.dk>
2024-07-06 18:53:19 +0200

    Update project's code

--- main.py
+++
@@ -1,5 +0,0 @@
-from src import main
-import sys
-
-if __name__ == "__main__":
-    sys.exit(main.main())
--- src/window.py
+++ src/window.py
@@ -1,12 +1,34 @@
-from gi.repository import Gtk
+from gi.repository import Adw, Gtk, Gio, GLib
+from pathlib import Path
 
 
-@Gtk.Template(resource_path="./window.ui")
-class MainWindow(Gtk.ApplicationWindow):
-    __gtype_name__ = "Window"
+@Gtk.Template(filename=str(Path("ui/window.ui").resolve()))
+class MainWindow(Adw.ApplicationWindow):
+    __gtype_name__ = "MainWindow"
+
+    image_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.set_default_size(200, 200)
-        self.connect("destroy", Gtk.main_quit)
-        self.show_all()
+
+        open_image_action = Gio.SimpleAction(name="open_image")
+        open_image_action.connect("activate", self.open_file_dialog)
+        self.add_action(open_image_action)
+
+    def set_image(self, source, result, *args):
+        try:
+            file = self.file_dialog.open_finish(result)
+
+            if file is not None:
+                print(file.get_path())
+
+        except GLib.Error:
+            print("Operation was cancelled.")
+
+    def open_file_dialog(self, *args):
+        self.file_dialog = Gtk.FileDialog(
+            title="Open Image",
+            default_filter=Gtk.FileFilter(name="images", mime_types=["image/*"]),
+            modal=True,
+        )
+        self.file_dialog.open(self, None, self.set_image)
